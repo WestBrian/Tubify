38de3d1c5759eff3ff8ab2fe3282c7434c15883c
WestBrian <brianwest@client-75-102-117-27.mobility-up.psu.edu>
2015-09-15 19:09:36 -0400

    Folder rearrange

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./backend/routes/index');
+var users = require('./backend/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'frontend'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/assets')));
 
 app.use('/', routes);
 app.use('/users', users);
