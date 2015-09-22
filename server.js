87b661c3659ce0f830ea1ff8cf17cd975a16997a
WestBrian <brianwest@client-75-102-121-42.mobility-up.psu.edu>
2015-09-22 19:06:13 -0400

    Commented server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 'use strict';
 
+// Dependencies
 var express = require('express');
 var app = express();
 
+// Routes
 var indexRoute = require('./server/routes/index_route');
 
+// Middleware
 app.use(indexRoute); //app.use('/', indexRoute);
 
+// Starting server
 var server = app.listen(3000, function(){
 	var host = server.address().address;
 	var port = server.address().port;
