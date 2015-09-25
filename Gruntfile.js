d97b8e75d6b2e11bcaa9b5d3571b70833fb5e116
WestBrian <brianwest@client-75-102-102-200.mobility-up.psu.edu>
2015-09-25 14:47:24 -0400

    Add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(grunt){
+	grunt.config.init({
+		concurrent: {
+			target: {
+				tasks: ['nodemon', 'watch'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
+		nodemon: {
+			dev: {
+				script: 'server.js'
+			}
+		},
+		stylus: {
+			all: {
+				files: {
+					'public/core/main.css': 'public/core/*.styl'
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['public/core/*.styl'],
+				tasks: ['stylus']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['concurrent']);
+
+};
--- server/routes/index_route.js
+++ server/routes/index_route.js
@@ -3,15 +3,9 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+
 router.get('/', function(req, res, next){
-
-
-res.render('layout');
-
+	res.render('layout');
 });
 
-router.get('/search.js', function(req, res, next){
-//res.sendFile(__dirname + '/../../public/core/search.js')   //this doesnt work because ../../ could be malicious (security flaw)
-res.sendFile(path.join(__dirname, '/../../public/core', 'search.js'));
-});
 module.exports = router;
