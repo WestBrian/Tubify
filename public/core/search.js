6f66840983359552f363d31260f439b3de99139b
13wrightd <13wrightd@gmail.com>
2015-09-25 04:32:41 -0400

    added search.js

---
+++ public/core/search.js
@@ -0,0 +1,63 @@
+// Searches youtube api for video
+function searchVideo(textInput){
+    var q = textInput;
+    var request = gapi.client.youtube.search.list({
+        q: textInput,
+        part: 'snippet',
+        type: 'video'
+    })
+
+    request.execute(showResponse);
+}
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    storeVideoInfo(response);
+
+    var mostRecentVideoId = response.items[0].id.videoId;
+    
+    //player.loadVideoById(mostRecentVideoId);
+    console.log('Video loaded.');
+}
+
+// Storing current video in an array to be updated to database
+ var dbvideoID = 0;
+function storeVideoInfo(response){
+   
+    var videoTitle = response.items[0].snippet.title;
+    var videoID1 = response.items[0].id.videoId;
+    var videoID2 = response.items[1].id.videoId;
+    var videoID3 = response.items[2].id.videoId;
+
+    var addedVid = []; // create an empty array
+    addedVid={
+        "URL1": response.items[0].id.videoId,
+        "URL2": response.items[1].id.videoId,
+        "URL3": response.items[2].id.videoId,
+        "searchTerm": searchTermText ,//searchTermText,
+        "title": videoTitle,
+        "playlistName": $('#playlist-search').val()
+    };
+    
+    localStorage.setItem('addedVid', JSON.stringify(addedVid));
+    $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "./php/add.php",
+                cache: false,
+                data:{myData:JSON.parse(localStorage.getItem('addedVid'))},// {myData:dataString},
+                success: function(data){
+                    console.log("video id is: " + data);
+                    dbvideoID = data;
+                },
+                error: function(e){
+                    console.log(e);
+                    console.log("aaaaaaaaaaaaaaaaaaaaaaa");
+                }
+            });
+    
+    var currentVideo = [dbvideoID, videoID1, videoID2, videoID3, searchTermText, videoTitle];
+    currentPlaylist.push(currentVideo);
+    localStorage.setItem('currentPlaylist', JSON.stringify(currentPlaylist));
+    loadPlaylist(currentPlaylist);
+}
--- server/routes/index_route.js
+++ server/routes/index_route.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 router.get('/', function(req, res, next){
 
 
@@ -10,4 +10,8 @@ res.render('layout');
 
 });
 
+router.get('/search.js', function(req, res, next){
+//res.sendFile(__dirname + '/../../public/core/search.js')   //this doesnt work because ../../ could be malicious (security flaw)
+res.sendFile(path.join(__dirname, '/../../public/core', 'search.js'));
+});
 module.exports = router;
