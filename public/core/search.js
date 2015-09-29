c3e1aebf3adc23dcad9b239d97034805c1b64064
WestBrian <brianwest@client-75-102-103-113.mobility-up.psu.edu>
2015-09-28 22:28:20 -0400

    Added keyboard features

--- public/core/search.js
+++ public/core/search.js
@@ -1,20 +1,19 @@
 'use strict';
 
-var counter=0;
 var app = angular.module('tubify', []);
 
 app.controller('CoreController', function($scope){
     var searchText = $scope.searchField;
-    $scope.list1 = ['hey','sup'];
+    $scope.list1 = [];
     $scope.searchList = [];
     $scope.counter=0;
-    
+
     $scope.search = function(){
+        // Resetting variables
+        $scope.counter = 0;
         $scope.searchList = [];
         
-
         var query = $scope.searchField;
-        $scope.counter=$scope.counter+1;
 
         var request = gapi.client.youtube.search.list({
             q: query,
@@ -25,16 +24,39 @@ app.controller('CoreController', function($scope){
         if($scope.searchField != ''){
             request.execute(function(response){
                 console.log(response.items[0]);
-                $scope.searchList=[];
+                $scope.searchList = [];
                 for(var i = 0; i < Math.min(response.items.length, 3); i++){
                     var obj = {
                         title: response.items[i].snippet.title,
-                        thumb: response.items[i].snippet.thumbnails.medium.url
+                        thumb: response.items[i].snippet.thumbnails.medium.url,
+                        videoId: response.items[i].id.videoId
                     };
                     $scope.searchList.push(obj);
                 }
                 $scope.$apply();
             });
         }
+    };
+
+    $scope.displayKey = function($event){
+        if ($event.keyCode == 38) {
+            if ($scope.counter > 0) {
+                $scope.counter--;
+            }else{
+                $scope.counter = 2;
+            }
+        }
+        if ($event.keyCode == 40) {
+            if ($scope.counter < 2) {
+                $scope.counter++;
+            }else{
+                $scope.counter = 0;
+            }
+        }
+    };
+
+    $scope.playVideo = function(){
+        $scope.list1.push($scope.searchList[$scope.counter].title);
+        player.loadVideoById($scope.searchList[$scope.counter].videoId);
     };
 });
