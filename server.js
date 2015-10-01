1800f1093ae326068e55868c62e14a8c392e39bc
unknown <diw5233@POLLOCK201-41.win.psu.edu>
2015-10-01 16:29:45 -0400

    mongo insert

--- server.js
+++ server.js
@@ -10,13 +10,33 @@ var mongoose = require('mongoose');
 var indexRoute = require('./server/routes/index_route');
 
 // Database
-mongoose.connect('mongodb://'+ cred.user +':'+ cred.password +'@ds051833.mongolab.com:51833/tubify');
+//mongoose.connect('mongodb://'+ cred.user +':'+ cred.password +'@ds051833.mongolab.com:51833/tubify');
+mongoose.connect('mongodb://admin1:serverpass314@candidate.52.mongolayer.com:10606,candidate.53.mongolayer.com:10195/tubifydb?replicaSet=set-560d8cc12a0bd7185f001142');
 var db = mongoose.connection;
+
+
 db.on('error', console.error.bind(console, 'Error: '));
 db.once('open', function(){
 	console.log('Database connected.');
 });
 
+/*
+var video = require('./server/models/video.js');
+
+
+var a = new video({
+	title:'sup',
+	urlId:'123456'
+});
+
+a.save(function(err){
+	if(err){
+		console.log(':(');
+	}else{
+		console.log(':)');
+	}
+});
+*/
 // Middleware
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/public');
