66b80a916b3cdc747826f4dfdfd8ba8128358eee
WestBrian <brianwest@Brians-MacBook-Pro-2.local>
2016-01-28 23:23:59 -0500

    updated

--- public/chat/chat-controller.js
+++ public/chat/chat-controller.js
@@ -3,13 +3,24 @@
 app.controller('ChatController', function($scope) {
     $scope.users = ['user1', 'user2', 'user3'];
     $scope.messages = ['msg1', 'msg2'];
+    var scope = angular.element($("#main")).scope();
 
     $scope.getUsers = function() {};
     $scope.getMessages = function() {};
     $scope.saveMessage = function() {
-        var message = $scope.chatInput;
+        var messageObject = {
+            name: username || 'guest',
+            message: $scope.chatInput,
+            playlist: scope.playlistField
+        }
         $scope.chatInput = '';
-
-        $scope.messages.push(message);
+        scope.socket.emit('send message', messageObject);
+        //$scope.messages.push(message);
     };
+
+    // Socket functions
+    scope.socket.on('message', function(data) {
+        $scope.messages.push(data.name + ': ' + data.message);
+        $scope.$apply();
+    });
 });
