9bd2b2cd0fa648f28c74c85bbb838ac2ca914bfb
WestBrian <brianwest@client-104-38-90-207.mobility.psu.edu>
2016-01-28 21:53:07 -0500

    started chat controller

--- public/chat/chat-controller.js
+++ public/chat/chat-controller.js
@@ -1,5 +1,15 @@
 'use strict';
 
 app.controller('ChatController', function($scope) {
-    
+    $scope.users = ['user1', 'user2', 'user3'];
+    $scope.messages = ['msg1', 'msg2'];
+
+    $scope.getUsers = function() {};
+    $scope.getMessages = function() {};
+    $scope.saveMessage = function() {
+        var message = $scope.chatInput;
+        $scope.chatInput = '';
+
+        $scope.messages.push(message);
+    };
 });
